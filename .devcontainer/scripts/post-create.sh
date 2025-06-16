7d7607971c0d9d9e9732620b8241c04713351f47
Dane Weber <dane.weber@excella.com>
2025-06-16 21:59:30 +0000

    feat: API endpoints for each cluster

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -13,13 +13,19 @@ k3d cluster delete managed | tee -a ~/.status.log
 # Managed Cluster #
 ###################
 # Install the managed K3D cluster
-k3d cluster create --api-port=$(hostname -I | awk '{print $1}'):6550 --config .devcontainer/manifests/k3d-managed.yaml --wait | tee -a ~/.status.log
+k3d cluster create managed \
+  --api-port=$(hostname -I | awk '{print $1}'):6550 \
+  --config .devcontainer/manifests/k3d-managed.yaml \
+  --wait | tee -a ~/.status.log
 
 ####################
 # Platform Cluster #
 ####################
 # Install the K3D cluster for Argo CD
-k3d cluster create --config .devcontainer/manifests/k3d-platform.yaml --wait | tee -a ~/.status.log
+k3d cluster create platform \
+  --api-port=$(hostname -I | awk '{print $1}'):6551 \
+  --config .devcontainer/manifests/k3d-platform.yaml \
+  --wait | tee -a ~/.status.log
 
 # Make sure we're on the right context
 kubectx k3d-platform | tee -a ~/.status.log
@@ -41,7 +47,10 @@ helm install argocd argo/argo-cd \
 # DevTest-East Cluster #
 ########################
 # Install the K3D cluster for Argo CD
-k3d cluster create --config .devcontainer/manifests/k3d-devtest-east.yaml --wait | tee -a ~/.status.log
+k3d cluster create devtest-east \
+  --api-port=$(hostname -I | awk '{print $1}'):6552 \
+  --config .devcontainer/manifests/k3d-devtest-east.yaml \
+  --wait | tee -a ~/.status.log
 
 # Make sure we're on the right context
 kubectx k3d-devtest-east | tee -a ~/.status.log
@@ -63,7 +72,10 @@ helm install argocd argo/argo-cd \
 # Prod-East Cluster #
 #####################
 # Install the K3D cluster for Argo CD
-k3d cluster create --config .devcontainer/manifests/k3d-prod-east.yaml --wait | tee -a ~/.status.log
+k3d cluster create prod-east \
+  --api-port=$(hostname -I | awk '{print $1}'):6553 \
+  --config .devcontainer/manifests/k3d-prod-east.yaml \
+  --wait | tee -a ~/.status.log
 
 # Make sure we're on the right context
 kubectx k3d-prod-east | tee -a ~/.status.log
