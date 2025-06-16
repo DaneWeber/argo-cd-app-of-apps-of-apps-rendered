5df7d3baf55bd24f20b173a0be8e57363bd34e73
Dane Weber <dane.weber@excella.com>
2025-06-16 17:12:29 +0000

    feat: rename dev cluster to platform

--- .devcontainer/scripts/post-start.sh
+++ .devcontainer/scripts/post-start.sh
@@ -5,7 +5,7 @@ echo "post-start start" >>  ~/.status.log
 # this runs in background each time the container starts
 
 # Ensure kubeconfig is set up. 
-k3d kubeconfig merge dev --kubeconfig-merge-default | tee -a ~/.status.log 
+k3d kubeconfig merge platform --kubeconfig-merge-default | tee -a ~/.status.log 
 
 # Update the repo for the workshop
 
